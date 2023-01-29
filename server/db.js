8e0aedf3b23eee5d78bf667ffb70063324568ba8
TarunMendu <tarunmendu12@gmail.com>
2023-01-29 20:12:33 +0530

    final commit

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const MONGO_URL = process.env.MONGO_URL;
 mongoose.set('strictQuery', false);
 export default async () => {
-    mongoose.connect(MONGO_URL, {
+    return mongoose.connect(MONGO_URL, {
         useNewUrlParser: true,
     });
 }
